e16f628733bbd9d01f9e4c34d5230a4ca644c4be
nepallium <122187419+nepallium@users.noreply.github.com>
2025-07-27 18:08:08 -0400

    Add Player class

---
+++ src/scripts/Player.js
@@ -0,0 +1,8 @@
+import Gameboard from "../Gameboard";
+
+export default class Player {
+    constructor(playerType) {
+        this.gameboard = new Gameboard();
+        this.playerType = playerType;
+    }
+}
