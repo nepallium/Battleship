9ce189e046a1b61f6d6baeeea19241806551d19d
nepallium <122187419+nepallium@users.noreply.github.com>
2025-08-01 08:08:05 -0400

    Partial improvement of computer AI

--- src/scripts/computerPlayer.js
+++ src/scripts/computerPlayer.js
@@ -4,17 +4,50 @@ import gameState from "./gameState";
 
 // use user's board copy to know which coordinates to select from
 const coords = generateCoordsArr()
+let adjCoords = []
 export function computerMakeMove() {
     const size = coords.length
-    // random idx
-    const idx = Math.floor(Math.random() * size)
 
-    const selectedCoord = coords.splice(idx, 1)[0]
-    
-    console.log(selectedCoord)
+    let idx, selectedCoord
+    if (adjCoords.length > 0) {
+        idx = Math.floor(Math.random() * adjCoords.length)
+        selectedCoord = adjCoords.splice(idx, 1)[0]
+    } else {
+        idx = Math.floor(Math.random() * size)
+        selectedCoord = coords.splice(idx, 1)[0]
+    }
+
     const selectedDiv = getDivFromCoord(selectedCoord)
 
     processAttack(selectedDiv, gameState.player2)
+
+    if (selectedDiv.classList.contains("hit")) {
+        findAdjCoords(selectedCoord)
+        console.log(adjCoords)
+        setTimeout((() => computerMakeMove()), 250)
+    }
+}
+
+function findAdjCoords(base) {
+    const [baseX, baseY] = base
+
+    adjCoords.push(...coords.filter(coord => {
+        const [x, y] = coord
+
+        if (x === baseX) {
+            if (y === baseY + 1 || y === baseY - 1) {
+                return true
+            }
+        }
+        if (y === baseY) {
+            if (x === baseX + 1 || x === baseX - 1) {
+                return true
+            }
+        }
+        else {
+            return false
+        }
+    }))
 }
 
 function generateCoordsArr() {
